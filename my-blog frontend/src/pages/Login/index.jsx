801c08f0955b0660eb2505731d6e605e99ab898f
Saoa35 <saoamah@gmail.com>
2023-05-26 19:47:00 +0400

    added useForm hook with methods

--- my-blog frontend/src/pages/Login/index.jsx
+++ my-blog frontend/src/pages/Login/index.jsx
@@ -5,8 +5,16 @@ import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 
 import styles from "./Login.module.scss";
+import { useForm } from "react-hook-form";
 
 export const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isValid },
+  } = useForm({ defaultValues: { email: "", password: "" } });
+
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
